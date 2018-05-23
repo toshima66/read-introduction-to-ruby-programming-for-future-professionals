a3eecf2f87b8626b54aa396ae87dd882e232fcca
toshima66 <t.oshima.66@gmail.com>
2018-05-23 20:59:48 +0900

    fizz buzz

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,2 +1,29 @@
-puts 'Hello, world!'
+def fizz_buzz(n)
+  if n % 15 == 0
+    'FizzBuzz'
+  elsif n % 5 == 0
+    'Buzz'
+  elsif n % 3 == 0
+    'Fizz'
+  else
+    n.to_s
+  end
+end
 
+puts fizz_buzz(1)
+puts fizz_buzz(2)
+puts fizz_buzz(3)
+puts fizz_buzz(4)
+puts fizz_buzz(5)
+puts fizz_buzz(6)
+puts fizz_buzz(7)
+puts fizz_buzz(8)
+puts fizz_buzz(9)
+puts fizz_buzz(10)
+puts fizz_buzz(11)
+puts fizz_buzz(12)
+puts fizz_buzz(13)
+puts fizz_buzz(14)
+puts fizz_buzz(15)
+puts fizz_buzz(16)
+puts fizz_buzz(17)
